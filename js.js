d0cc31b348da09a789e02eff5256814448e15c05
welinghton <welinghtonjogos@gmail.com>
2023-08-23 02:39:21 -0300

    BACANA

--- js.js
+++ js.js
@@ -1,14 +1,15 @@
+var email, senha, texto;
 
-var email, senha, texto
-
-
-function enviar(){
-   email = document.getElementById("EMAIL").value
- senha = document.getElementById("NOME").value
- texto = document.getElementById("TEXTO").value
-  if(email != "" && senha != "" && texto != ""){
-    alert('DEU CERTO')
-  }else{
-  alert('Email ou Senha estao vazios')
-}
+function enviar() {
+  email = document.getElementById("EMAIL").value;
+  senha = document.getElementById("NOME").value;
+  texto = document.getElementById("TEXTO").value;
+  if (email != "" && senha != "" && texto != "") {
+    alert("DEU CERTO");
+  } else {
+    alert("Email ou Senha estao vazios");
+    document.getElementById("EMAIL").style.borderBottom = "4px solid Crimson";
+    document.getElementById("NOME").style.borderBottom = "4px solid Crimson";
+    document.getElementById("TEXTO").style.borderBottom = "4px solid Crimson";
+  }
 }
